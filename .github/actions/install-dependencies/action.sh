f47f3fe8ce8ab5f3da903dfc2fd160812b836537
Ahmed ElBakry <aelbakry.wayfair.com>
2022-05-05 17:29:10 +0200

    Update the tests

--- .github/actions/install-dependencies/action.sh
+++ .github/actions/install-dependencies/action.sh
@@ -3,6 +3,11 @@ set -euo pipefail
 echo "Ensuring pip is up to date"
 python -m pip install --upgrade pip
 
+if [[ "${INSTALL_REQUIREMENTS}" == "true"  ]]; then
+  echo "Installing requirements"
+  pip install -r requirements.txt
+fi
+
 if [[ "${INSTALL_TEST_REQUIREMENTS}" == "true"  ]]; then
   echo "Installing test requirements"
   pip install -r requirements-test.txt
